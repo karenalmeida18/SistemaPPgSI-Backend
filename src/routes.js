3f4cc5f44a3c2890b13374681e848eb07522d82f
karen <karenkaka2010@hotmail.com>
2021-08-16 19:16:27 -0300

    build: add eslint and husky

--- src/routes.js
+++ src/routes.js
@@ -2,8 +2,6 @@ const { Router } = require('express');
 
 const routes = new Router();
 
-routes.get('/', (req, res) => {
-  return res.json({ message: 'Hello world' });
-});
+routes.get('/', (req, res) => res.json({ message: 'Hello world' }));
 
 module.exports = routes;
