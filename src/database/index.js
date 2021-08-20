244ca19067d9cb939386ad46d8721acb690f44fd
maykborgs <maaykon51@gmail.com>
2021-08-19 22:13:25 -0300

    feat: user model and controller

--- src/database/index.js
+++ src/database/index.js
@@ -1,8 +1,11 @@
 const Sequelize = require('sequelize');
 const dbConfig = require('../config/database');
+const User = require('../models/User');
 
 const connection = new Sequelize(dbConfig);
 
 connection.authenticate().then(() => console.log('conexão criada'));
+
+User.init(connection);
 
 module.exports = connection;
