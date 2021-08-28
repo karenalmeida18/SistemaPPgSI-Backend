3ee19638d586e717d68cb87dbd1b288971ede24f
karen <karenkaka2010@hotmail.com>
2021-08-28 13:04:32 -0300

    build: add cors

--- src/server.js
+++ src/server.js
@@ -3,12 +3,14 @@ require('dotenv').config({
 });
 require('./database');
 
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes');
 
 const app = express();
 
 app.use(express.json());
+app.use(cors);
 app.use(routes);
 
 module.exports = app.listen(process.env.PORT || 3333);
