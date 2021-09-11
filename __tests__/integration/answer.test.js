3dbbb7c14a2d5d88b25d9db26cc240a7cd3475ef
Gabriela Sinastre <gabrielamaria.gaby@hotmail.com>
2021-09-11 15:03:48 -0300

    test: add answer + form + question

---
+++ __tests__/integration/answer.test.js
@@ -0,0 +1,87 @@
+const request = require('supertest');
+const app = require('../../src/server');
+
+describe('Testing question crud ', () => {
+  afterEach(() => {
+    app.close();
+  });
+
+  it('Should create a new answer to the question', async () => {
+    await request(app)
+      .post('/user/create')
+      .send({
+        name: 'Gabriela', usp_code: '20', user_type: 'ccp', password: '456',
+      });
+
+    const { body: { token, id } } = await request(app)
+      .post('/user/login')
+      .send({
+        usp_code: '20', password: '456',
+      });
+
+    await request(app)
+      .post('/form/create')
+      .set('Authorization', `Bearer ${token}`);
+
+    await request(app)
+      .post('/question/create/1')
+      .send({
+        description: 'testando', form_id: '1',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    const { status } = await request(app)
+      .post('/answer/create/1')
+      .send({
+        answer: 'resposta', usp_code: `${id}`, question_id: '1',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(status).toBe(200);
+  });
+
+  it('Should be able to find all the questions of a form', async () => {
+    await request(app)
+      .post('/user/create')
+      .send({
+        name: 'Gabriela', usp_code: '20', user_type: 'ccp', password: '456',
+      });
+
+    const { body: { token, id } } = await request(app)
+      .post('/user/login')
+      .send({
+        usp_code: '20', password: '456',
+      });
+
+    await request(app)
+      .post('/form/create')
+      .set('Authorization', `Bearer ${token}`);
+
+    await request(app)
+      .post('/question/create/1')
+      .send({
+        description: 'testando', form_id: '1',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    await request(app)
+      .post('/answer/create/1')
+      .send({
+        answer: 'resposta', usp_code: `${id}`, question_id: '1',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    const { status } = await request(app)
+      .get('/answer/read/1')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(status).toBe(200);
+  });
+
+  /* it('Should list all users', async () => {
+    const response = await request(app)
+      .get('/user/read');
+
+    expect(response.status).toBe(200);
+  }); */
+});
---
+++ __tests__/integration/question.test.js
@@ -0,0 +1,73 @@
+const request = require('supertest');
+const app = require('../../src/server');
+
+describe('Testing question crud ', () => {
+  afterEach(() => {
+    app.close();
+  });
+
+  it('Should create a new form question', async () => {
+    await request(app)
+      .post('/user/create')
+      .send({
+        name: 'Gabriela', usp_code: '20', user_type: 'ccp', password: '456',
+      });
+
+    const { body: { token } } = await request(app)
+      .post('/user/login')
+      .send({
+        usp_code: '20', password: '456',
+      });
+
+    await request(app)
+      .post('/form/create')
+      .set('Authorization', `Bearer ${token}`);
+
+    const { status } = await request(app)
+      .post('/question/create/1')
+      .send({
+        description: 'testando', form_id: '1',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(status).toBe(200);
+  });
+
+  it('Should be able to find all the questions of a form', async () => {
+    await request(app)
+      .post('/user/create')
+      .send({
+        name: 'Gabriela', usp_code: '20', user_type: 'ccp', password: '456',
+      });
+
+    const { body: { token } } = await request(app)
+      .post('/user/login')
+      .send({
+        usp_code: '20', password: '456',
+      });
+
+    await request(app)
+      .post('/form/create')
+      .set('Authorization', `Bearer ${token}`);
+
+    await request(app)
+      .post('/question/create/1')
+      .send({
+        description: 'testando', form_id: '1',
+      })
+      .set('Authorization', `Bearer ${token}`);
+
+    const { status } = await request(app)
+      .get('/question/index/1')
+      .set('Authorization', `Bearer ${token}`);
+
+    expect(status).toBe(200);
+  });
+
+  /* it('Should list all users', async () => {
+    const response = await request(app)
+      .get('/user/read');
+
+    expect(response.status).toBe(200);
+  }); */
+});
